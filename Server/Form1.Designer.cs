6f676cce97ff359a2bff39273b813fd36640256c
Saberslay <dansteamshell@gmail.com>
2022-06-29 13:32:37 +0100

    Started work on the Server side

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CMD_BTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CMD_BTN
+            // 
+            this.CMD_BTN.Location = new System.Drawing.Point(659, 366);
+            this.CMD_BTN.Name = "CMD_BTN";
+            this.CMD_BTN.Size = new System.Drawing.Size(105, 49);
+            this.CMD_BTN.TabIndex = 0;
+            this.CMD_BTN.Text = "Send test Command";
+            this.CMD_BTN.UseVisualStyleBackColor = true;
+            this.CMD_BTN.Click += new System.EventHandler(this.CMD_BTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 427);
+            this.Controls.Add(this.CMD_BTN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CMD_BTN;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server {
+    public partial class Form1 : Form {
+
+        public static bool ISAlarmeON = false;
+
+        private static byte[] _buffer = new byte[1024];
+        private static List<Socket> _ClientSockets = new List<Socket>();
+        private static Socket _ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public Form1() {
+            InitializeComponent();
+            SetupServer();
+        }
+
+        private static void SetupServer() {
+            _ServerSocket.Bind(new IPEndPoint(IPAddress.Any, 1996));
+            _ServerSocket.Listen(1);
+            _ServerSocket.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+        }
+
+        private static void AcceptCallBack(IAsyncResult AR) {
+            Socket socket = _ServerSocket.EndAccept(AR);
+            _ClientSockets.Add(socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallBack), socket);
+            _ServerSocket.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+        }
+        private static void ReceiveCallBack(IAsyncResult AR) {
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] dataBuff = new byte[received];
+            Array.Copy(_buffer, dataBuff, received);
+            string text = Encoding.ASCII.GetString(dataBuff);
+
+            string Response = string.Empty;
+
+            if (text.ToLower() != "ISAlarmeON") {
+                Response = "Invalid";
+            } else {
+                if (ISAlarmeON == true) {
+                    Response = "yes";
+                } else {
+                    Response = "no";
+                }
+            }
+
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallBack), socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallBack), socket);
+        }
+
+        private static void SendCallBack(IAsyncResult AR) {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+
+        private void CMD_BTN_Click(object sender, EventArgs e) {
+            ISAlarmeON = true;
+        }
+    }
+}
